e19c7a11fca95bb9af731e07dc57210621cd6d2e
BatraAayush <67547222+BatraAayush@users.noreply.github.com>
2024-10-04 00:41:43 +0530

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import deviceDataRoutes from "./routes/deviceDataRoutes.js";
 
@@ -7,11 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", deviceDataRoutes);
 
 app.get("/", (req, res) => res.send("Dashboard's Backend"));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server running on http://localhost:3000");
 });
